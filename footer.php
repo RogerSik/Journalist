fa96e7d0319e839750e9217b9b19efb8fb76f6c0
Roger <roger@sikorski.cc>
2015-06-13 15:57:15 +0200

    Changed url yoschi.cc to sikorski.cc

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 
 <div id="footer">
-	<p>The Journalist template by <a href="http://lucianmarin.com/" rel="designer">Lucian E. Marin</a> - enhanced by <a href="http://yoschi.cc/">Roger Sikorski</a> &mdash; Built for <a href="http://wordpress.org/">WordPress</a></p>
+	<p>The Journalist template by <a href="http://lucianmarin.com/" rel="designer">Lucian E. Marin</a> - enhanced by <a href="http://sikorski.cc/">Roger Sikorski</a> &mdash; Built for <a href="http://wordpress.org/">WordPress</a></p>
 </div>
 <?php do_action('wp_footer', ''); ?>
 
